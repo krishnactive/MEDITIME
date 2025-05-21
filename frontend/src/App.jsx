508faa5de85589cb005fc753d908c9afeda72dd5
Krishna kant sharma <krishnactive@gmail.com>
2025-05-21 18:28:06 +0530

    navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,25 @@ import Doctors from './pages/Doctors'
 import MyProfile from './pages/MyProfile'
 import About from './pages/About'
 import Contact from './pages/Contact'
-
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/login' element={<Login/>} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
     </div>
   )
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5f6fff", // Your custom primary color
+      },
+    },
+  },
+  plugins: [],
+};
