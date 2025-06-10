64941607ed6e32b9fa469f97545475caa1bf86fc
Krishna kant sharma <krishnactive@gmail.com>
2025-06-10 13:26:37 +0530

    MyProfile page done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                 <li className='py-1'>ABOUT</li>
                 <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink to='/contact'>
+            <NavLink to='contact'>
                 <li className='py-1'>CONTACT</li>
                 <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
@@ -41,7 +41,7 @@ const Navbar = () => {
                     </div>
                     <div className='absolute top-0 right-0 pt-14 text-base font-medium text-medium text-gray-600 z-20 hidden group-hover:block'>
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'> 
-                            <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                            <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                             <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
                             <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
                         </div>
