b5509e10c12142134e33cd35952f59899d6171b4
Krishna kant sharma <krishnactive@gmail.com>
2025-06-11 12:35:25 +0530

    backend set up simple express app checking

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from "cors"
+import 'dotenv/config'
+
+
+// app config
+const app = express();
+const PORT = process.env.PORT||4000
+
+//middlewares
+app.use(express.json())
+app.use(cors());
+
+
+// api end points
+
+app.get('/', (req, res)=>{
+    res.send("api working")
+})
+
+app.listen(PORT, ()=>console.log(`Server started at ${PORT}`))
