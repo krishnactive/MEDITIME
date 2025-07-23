0320eb687a4197a66a4caec1a986015d3c1a724a
Krishna kant sharma <krishnactive@gmail.com>
2025-07-23 22:54:57 +0530

    verify admin to add doctor

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 
 import { addDoctor, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/authAdmin.js";
 
 const adminRouter = express.Router();
 
 //multiple end points
-adminRouter.post('/add-doctor', upload.single('file'), addDoctor);
+adminRouter.post('/add-doctor',authAdmin, upload.single('file'), addDoctor);
 adminRouter.post('/login', loginAdmin);
 
 export default adminRouter;
