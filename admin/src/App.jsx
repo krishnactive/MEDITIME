6691d23b2877b2905433c4488073cfa1513a127f
Krishna kant sharma <krishnactive@gmail.com>
2025-07-24 18:35:42 +0530

    admin panel setup

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      krishna
+      <h1 class="text-3xl font-bold underline bg-blue-50 text-blue-500 align-left">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
