0bee3eee42cb0cf16ae3245fd6bbfc380c19c2fd
Krishna kant sharma <krishnactive@gmail.com>
2025-07-27 05:08:19 +0530

    some routes refactor

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link, useLocation } from 'react-router-dom';
 
 const Footer = () => {
+    const location = useLocation();
+
+  const handleScrollToTop = () => {
+    if (location.pathname !== '/') {
+      setTimeout(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }, 0);
+    } else {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
   return (
     <div className='md:mx-10'>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
@@ -18,10 +30,10 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>COMPANY</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
-                    <li>Privacy policy</li>
+                    <li><Link to="/" onClick={handleScrollToTop}>Home</Link></li>
+                    <li><Link to="/about" onClick={handleScrollToTop}>About us</Link></li>
+                    <li><Link to="/contact" onClick={handleScrollToTop}>Contact us</Link></li>
+                    {/* <li><Link to="/privacy" onClick={handleScrollToTop}>Privacy policy</Link></li> */}
                 </ul>
 
             </div>
